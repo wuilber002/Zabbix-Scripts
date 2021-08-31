c25fccf5610e90b570e43623fb51bc5772965614
Igor Nicoli <wuilber002@users.noreply.github.com>
2021-08-31 11:53:16 -0300

    script de instacao do zabbix agent 2

---
+++ install/install-zbxAgent2_oracle-linux-8-x86_64.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+#===============================================================================
+#         FILE: install-zbxAgent2_oracle-linux-8-x86_64.sh
+#        USAGE: curl -ks https://raw.githubusercontent.com/wuilber002/Zabbix-Scripts/master/install/install-zbxAgent2_oracle-linux-8-x86_64.sh|bash -s -- <zabbix_server_address>
+#
+#  DESCRIPTION: Script de instalacao para o Zabbix Agent2 5.4 
+#
+#       AUTHOR: Igor Nicoli
+#      VERSION: 1.0
+#      CREATED: 30/08/2021 17:55:01 PM
+#     REVISION: ---
+#===============================================================================
+
+GREEN='\033[0;32m' # Green color
+RED='\033[0;31m'   # Red color
+RESET='\033[0m'    # Reset color
+
+# Endereco Ip do Zabbix Server e Data de backup para os arquivos de configuracao.
+zabbix_server=$1
+backup_data=$(date +%Y_%m_%d-%H_%M_%S)
+log="/tmp/zabbix_agent_install_${backup_data}.log"
+if [ -z "${zabbix_server}" ]; then
+    printf "[${RED}ERRO${RESET}] Voce precisa especificar o endereco do servidor zabbix.\n" 2>&1 | tee --append ${log}
+    exit 1
+fi
+
+printf "[${GREEN}INFO${RESET}] Configuracao do repositorio do Zabbix\n" 2>&1 | tee --append ${log}
+rpm -Uvh https://repo.zabbix.com/zabbix/5.4/rhel/8/x86_64/zabbix-release-5.4-1.el8.noarch.rpm 1>>${log} 2>&1
+
+printf "[${GREEN}INFO${RESET}] Instalando o Zabbix agent2\n" 2>&1 | tee --append ${log}
+yum --assumeyes install zabbix-sender zabbix-agent2 1>>${log} 2>&1
+
+printf "[${GREEN}INFO${RESET}] Configuracao de regras de Firewall (Oracle Linux)\n" 2>&1 | tee --append ${log}
+firewall-cmd --zone=public --permanent --add-port=10050/tcp 1>>${log} 2>&1
+firewall-cmd --reload 1>>${log} 2>&1
+iptables -nL 1>>${log} 2>&1
+
+printf "[${GREEN}INFO${RESET}] Configurando o Zabbix agent2\n" 2>&1 | tee --append ${log}
+cp /etc/zabbix/zabbix_agent2.conf /etc/zabbix/zabbix_agent2.conf_${backup_data}
+sed -i -e "s/Server=127.0.0.1$/Server=${zabbix_server}/" \
+-e "s/# Plugins.SystemRun.LogRemoteCommands=0/Plugins.SystemRun.LogRemoteCommands=1/" \
+-e "s/# DenyKey=system.run[*]/AllowKey=system.run[*]/" \
+-e "s/# UnsafeUserParameters=0/UnsafeUserParameters=1/" \
+-e "s/^Hostname=Zabbix server$/Hostname=$(hostname -s|cut -d \. -f 1|tr [a-z] [A-Z])/" \
+/etc/zabbix/zabbix_agent2.conf
+cat /etc/zabbix/zabbix_agent2.conf|egrep '^(Server|LogRemoteCommands|DenyKey|UnsafeUserParameters|Hostname)=' 1>>${log} 2>&1
+
+printf "[${GREEN}INFO${RESET}] Iniciando o servico do zabbix agent2\n" 2>&1 | tee --append ${log}
+systemctl enable zabbix-agent2.service 1>>${log} 2>&1
+systemctl start zabbix-agent2.service 1>>${log} 2>&1
+systemctl status zabbix-agent2.service 2>&1 | tee --append ${log}
+lsof -Pi tcp:10050 +c0 2>&1 | tee --append ${log}
+
+### Uninstall commands:
+rpm -aq | grep zabbix|xargs yum --assumeyes remove
+rm -rf /etc/zabbix
+firewall-cmd --zone=public --remove-port=10050/tcp --permanent && firewall-cmd --reload
---
+++ install/install-zbxAgent2_windows-x86_64.ps1
@@ -0,0 +1,77 @@
+#===============================================================================
+#         FILE: install-zbxAgent2_windows-x86_64.ps1
+#        USAGE: curl -ks https://raw.githubusercontent.com/wuilber002/Zabbix-Scripts/master/install/install-zbxAgent2_windows-x86_64.ps1
+#
+#  DESCRIPTION: Script de instalacao para o Zabbix Agent2 5.4 
+#
+#       AUTHOR: Igor Nicoli
+#      VERSION: 1.0
+#      CREATED: 30/08/2021 17:55:01 PM
+#     REVISION: ---
+#===============================================================================
+#
+# Para instalar esse script, execute os dois comandos abaixo:
+# $install_script="https://raw.githubusercontent.com/wuilber002/Zabbix-Scripts/master/install/install-zbxAgent2_windows-x86_64.ps1"
+# Invoke-WebRequest -Uri $install_script -OutFile "$env:TEMP\zabbix_agent_install.ps1"
+
+# Parametros Ip do Zabbix Server e Data de backup para os arquivos de configuracao.
+param ($server, $port='')
+if ($server -eq $null) {
+    Write-Host "Voce precisa informar o endereco do servidor zabbix."
+    exit 1
+}
+$msi='https://cdn.zabbix.com/zabbix/binaries/stable/5.4/5.4.3/zabbix_agent2-5.4.3-windows-amd64-openssl.msi'
+$install_folder = 'C:\Program Files\Zabbix Agent'
+$server_name = (Get-WMIObject Win32_ComputerSystem | Select-Object -ExpandProperty name).ToUpper()
+$DataStamp = get-date -Format yyyy.MM.dd-HH.mm.ss
+$logFile = '{0}\{1}-{2}.log' -f $env:TEMP,"install-zabbix-agent",$DataStamp
+
+# Download do binario de instalacao:
+Add-Content -Path "FilePath" -Value "Fazendo download do instalador"
+Write-Host 'Fazendo download do instalador'
+Invoke-WebRequest -Uri $msi -OutFile "$env:TEMP\zabbix_agent.msi" 
+
+# Comando de instalacao do Zabbix Agent 2
+if (Test-Path "$env:TEMP\zabbix_agent.msi") {
+    Write-Host 'Instalando o Zabbix Agent 2'
+    $MSIArguments = @(
+        "/passive"
+        "/norestart"
+        "/l*v ""$logFile"""
+        "/i ""$env:TEMP\zabbix_agent.msi"""
+        "ADDLOCAL=""AgentProgram,MSIPackageFeature"""
+        "LOGTYPE=""file"""
+        "LOGFILE=""$install_folder\log\zabbix_agentd.log"""
+        "ENABLEREMOTECOMMANDS=""1"""
+        "SERVER=""$server,127.0.0.1"""
+        "SERVERACTIVE=""$server$port"""
+        "HOSTNAME=""$server_name"""
+        "TIMEOUT=""15"""
+        "INSTALLFOLDER=""$install_folder"""
+        "ENABLEPATH=""1"""
+        "SKIP=""fw"""
+    )
+    Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait
+
+    # deleta o binario de instalacao do Zabbix Agent 2
+    Remove-Item -path "$env:TEMP\zabbix_agent.msi" -recurse
+}
+
+# Firewall rules (Oracle Linux)
+Write-Host '>>> Criando regra de firewall'
+New-NetFirewallRule -DisplayName "Zabbix Agent" -Direction inbound -Profile Any -Action Allow -LocalPort 10050 -Protocol TCP | Out-File -Append -FilePath "$logFile"
+
+# Iniciar o servico do Zabbix Agent 2
+Write-Host '>>> Iniciando o servico'
+Start-Service -Name "Zabbix Agent 2" | Out-File -Append -FilePath "$logFile"
+
+Write-Host ">>> Instalation information <<<"
+Write-Host "> Hostname = $server_name"
+Write-Host "> Local Address ="(Test-Connection -ComputerName (hostname) -Count 1  | Select -ExpandProperty IPV4Address).IPAddressToString
+Write-Host "> Zabbix Server = $server$port"
+Write-Host "> Instalation Folder = $install_folder"
+
+
+### Uninstall commands:
+(Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Zabbix Agent 2 (64-bit)"}).Uninstall()
+Remove-NetFirewallRule -DisplayName "Zabbix Agent"
