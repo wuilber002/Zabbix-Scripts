a772569e39134498d2ef802de3f0349811d911fa
Igor Nicoli <wuilber002@users.noreply.github.com>
2021-08-31 11:56:43 -0300

    Update install-zbxAgent2_oracle-linux-8-x86_64.sh

--- install/install-zbxAgent2_oracle-linux-8-x86_64.sh
+++ install/install-zbxAgent2_oracle-linux-8-x86_64.sh
@@ -16,7 +16,7 @@ RED='\033[0;31m'   # Red color
 RESET='\033[0m'    # Reset color
 
 if [ `id -u` -ne 0 ]; then
-    printf '[ERRO] Voce precisa esta logado como root... Execute o comando "sudo su -"'
+    printf '[${RED}ERRO${RESET}] Voce precisa esta logado como root... Execute o comando "sudo su -"'
     exit 1
 fi
 
@@ -57,6 +57,6 @@ systemctl status zabbix-agent2.service 2>&1 | tee --append ${log}
 lsof -Pi tcp:10050 +c0 2>&1 | tee --append ${log}
 
 ### Uninstall commands:
-# rpm -aq | grep zabbix|xargs yum --assumeyes remove
-# rm -rf /etc/zabbix
-# firewall-cmd --zone=public --remove-port=10050/tcp --permanent && firewall-cmd --reload
+rpm -aq | grep zabbix|xargs yum --assumeyes remove
+rm -rf /etc/zabbix
+firewall-cmd --zone=public --remove-port=10050/tcp --permanent && firewall-cmd --reload
